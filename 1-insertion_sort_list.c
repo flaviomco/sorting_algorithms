218fa185a4ce72e10b0e4ba97b41942d9271097f
flaviomco <2109@holbertonschool.com>
2021-01-31 16:45:59 +0000

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,48 +1,53 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending
- * order using the Insertion sort algorithm
- * @list: list to be sorted
- * Return: void
+ * insertion_sort_list - Sorts an doubly linked list
+ * of integers in ascending order using
+ * the Insertion sort algorithm.
+ * @list: The doubly linked list to sort
+ *
+ * Return: Nothing!
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current; /* traverses list */
-	listint_t *temp; /* holds temp values */
+	bool flag = false;
+	listint_t *tmp = NULL, *aux = NULL;
 
-	current = *list;
-	current = current->next;
-
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (!list || !(*list) || !(*list)->next)
 		return;
 
-	while (current != NULL)
+	tmp = *list;
+	while (tmp->next)
 	{
-		while (current->prev != NULL && current->n < current->prev->n)
+		if (tmp->n > tmp->next->n)
 		{
-			temp = current;
+			tmp->next->prev = tmp->prev;
+			if (tmp->next->prev)
+				tmp->prev->next = tmp->next;
+			else
+				*list = tmp->next;
 
-			/* link next and previous nodes together */
-			if (current->next != NULL)
-				current->next->prev = temp->prev;
-			current->prev->next = temp->next;
+			tmp->prev = tmp->next;
+			tmp->next = tmp->next->next;
+			tmp->prev->next = tmp;
+			if (tmp->next)
+				tmp->next->prev = tmp;
 
-			/* move to previous node, insert temp node before it */
-			current = current->prev;
-			temp->prev = current->prev;
-			temp->next = current;
-			if (current->prev != NULL)
-				current->prev->next = temp;
-			current->prev = temp;
-
-			/* if node is new head of list, set it to *list */
-			if (temp->prev == NULL)
-				*list = temp;
-
+			tmp = tmp->prev;
 			print_list(*list);
-			current = current->prev;
+
+			if (tmp->prev && tmp->prev->n > tmp->n)
+			{
+				if (!flag)
+					aux = tmp->next;
+				flag = true;
+				tmp = tmp->prev;
+				continue;
+			}
 		}
-		current = current->next;
+		if (!flag)
+			tmp = tmp->next;
+		else
+			tmp = aux, flag = false;
 	}
 }
